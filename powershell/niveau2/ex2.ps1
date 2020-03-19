c3a38fa5f04d551f6ae65ef455a682677c643b76
Emilien Devos <contact@emiliendevos.be>
2020-03-19 14:29:15 +0100

    end of ex2

--- powershell/niveau2/ex2.ps1
+++ powershell/niveau2/ex2.ps1
@@ -1,13 +1,27 @@
 # it is needed to launch this script as an administrator
 
-# FIRST PART
-
+$date = $(get-date -f yyyy-MM-dd-H-m-s)
 $registryPath = "HKLM:\SOFTWARE"
 # this is a hashtable, similar to a dictionnary in python
 $registryKeys = @{ "Test-Version" = "2.0"; "Test-Region" = "Belgium"}
-foreach($key in $registryKeys.keys)
-{
-    New-ItemProperty -Path $registryPath -Name $key -Value $registryKeys[$key]  -PropertyType "String"
+
+function Set-RegKey {
+    param( [String]$regPath, [String]$keyName, [String]$keyValue, [String]$keyType )
+    New-ItemProperty -Path $regPath -Name $keyName -Value $keyValue  -PropertyType $keyType
 }
 
-# SECOND PART
+function Write-Log {
+    param([String]$logString)
+    New-Item -ItemType Directory -Force -Path "C:\logs"
+    $logString | Out-File "C:\logs\$date.txt" -Append
+}
+
+Write-Log "Début du script"
+
+foreach($key in $registryKeys.keys)
+{
+    $result = Set-RegKey $registryPath $key $registryKeys[$key] "String"
+    Write-Log $result
+}
+
+Write-Log "Fin du script"
